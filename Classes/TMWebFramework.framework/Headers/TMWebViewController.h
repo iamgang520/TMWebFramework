58b64b508c4754bbc5bb2dce8f237643fa90fbae
iamgang <5391519@qq.com>
2018-10-16 13:31:54 +0800

    创建版本 0.0.1

---
+++ Classes/TMWebFramework.framework/Headers/TMWebViewController.h
@@ -0,0 +1,19 @@
+//
+//  TMWebViewController.h
+//  TMProject
+//
+//  Created by iamgang on 2018/10/8.
+//  Copyright © 2018年 Tianma. All rights reserved.
+//
+
+#import <TMSDK/TMSDK.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TMWebViewController : TMViewController
+
+@property (nonatomic, strong) NSString *urlStr;
+
+@end
+
+NS_ASSUME_NONNULL_END
